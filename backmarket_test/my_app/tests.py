88cb3ef079b6c50a383b9ab042d74c34dfd63742
qdufrois <quentin.dufrois@gmail.com>
2019-03-26 14:25:52 +0100

    Adding a form to get the city from a user

--- backmarket_test/my_app/tests.py
+++ backmarket_test/my_app/tests.py
@@ -12,4 +12,9 @@ class TestHome(TestCase):
     def test_home(self):
         """Testing if the home view is correctly generated"""
         response = self.client.get(reverse("my_app:home"))
-        self.assertTrue("<h1>Hello World!</h1>" in str(response.content))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("<h1>Welcome on the back market weather app</h1>" in str(response.content))
+    
+    def test_form(self):
+        response = self.client.post(reverse('my_app:home'), data={'city':'paris'})
+
--- backmarket_test/my_app/views.py
+++ backmarket_test/my_app/views.py
@@ -2,8 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from my_app.forms import CityForm
 
 
 def home(request):
     """View rendering the home page"""
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+
+        if form.is_valid():
+            city = form.cleaned_data["city"]
+            sent = True
+    
+    else:
+        form = CityForm()
+    
+
     return render(request, "my_app/home.html", locals())
